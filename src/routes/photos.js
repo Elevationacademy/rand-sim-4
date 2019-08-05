ba3bfacc49a7c11b4607c52fbd376414f97678d8
Omer Morad <omer.moradd@gmail.com>
2019-08-05 14:47:57 +0300

    Added minfied version

--- src/routes/photos.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import photosRestController from '../controllers/photos.rest-controller'
-
-const router = express.Router()
-
-router.get( '/search/:term', photosRestController.searchPhotos.bind( photosRestController ) )
-router.get( '/:photo_id', photosRestController.fetchPhotoInfo.bind( photosRestController ) )
-
-export default router
